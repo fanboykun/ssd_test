a5e29d8dff228afe68668920e0413de521996dd1
fanboykun <irfanramadhan1812@gmail.com>
2024-12-06 20:58:30 +0700

    update: move trust proxies configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,24 +23,6 @@ class AppServiceProvider extends ServiceProvider
     {
         if (config('app.env') === 'production') {
             URL::forceScheme('https');
-
-            // Trust Cloud Run's load balancer
-            Request::macro('isSecure', function () {
-                $isSecure = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on';
-                $hasForwardedProto = isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https';
-
-                return $isSecure || $hasForwardedProto;
-            });
-
-            // Trust all proxies in Cloud Run environment
-            Request::setTrustedProxies(
-                ['*'],
-                Request::HEADER_X_FORWARDED_FOR |
-                Request::HEADER_X_FORWARDED_HOST |
-                Request::HEADER_X_FORWARDED_PORT |
-                Request::HEADER_X_FORWARDED_PROTO |
-                Request::HEADER_X_FORWARDED_AWS_ELB
-            );
         }
     }
 }
