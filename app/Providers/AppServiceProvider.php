5fc66c91036c4619d8e7296e7e0827cf9f521785
fanboykun <irfanramadhan1812@gmail.com>
2024-12-06 18:40:58 +0700

    fix: mixed content, add: env prod key in .env.example

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+            \Illuminate\Support\Facades\URL::forceRootUrl(config('app.url'));
+        }
     }
 }
